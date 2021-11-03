8c7f5679c58f44fa3a192517b973f58fe91d1727
swetha-11-srisai <swethachowdary857@gmail.com>
2021-11-02 21:56:21 -0700

    [Swetha] add : Square project created

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareProblem
+{
+    class Class1
+    {
+        public void Check()
+        {
+            int i= 20;
+            float f = 45.5;
+            //double d = 75.789;
+            //char ch = abc;
+            Console.WriteLine("Hello swetha");
+            Console.WriteLine("i:{0},f:{0}");
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareProblem
+{
+    class Square
+    {
+        int a1=10;
+        int a2=10;
+        //public void Values()
+        //{
+          //  a1 = 4;
+            //a2 = 4;
+            
+        //}
+        public int GetArea()
+        {
+            return a1 * a2;
+        }
+        public void Display()
+        {
+            Console.WriteLine("side1:{0}", a1);
+            Console.WriteLine("side2:{0}", a2);
+            Console.WriteLine("Area of Square:{0}", GetArea());
+        }
+
+        static void Main(string[] args)
+        {
+            Square s = new Square();
+            Class1 c = new Class1();
+            //Assign a = new Assign(); 
+            //s.Values();
+            s.GetArea();
+            s.Display();
+            c.Check();
+           // a.Check();
+            Console.ReadLine();
+        }
+    }
+}
