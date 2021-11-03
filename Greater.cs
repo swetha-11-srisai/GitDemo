9dd6e91f7f382e1cd84ca03e20f6bd2c97cd7e19
swetha-11-srisai <swethachowdary857@gmail.com>
2021-11-02 22:02:46 -0700

    [Swetha] add : Loop program created

---
+++ Greater.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Greater
+    {
+        public void Method()
+        {
+            int a = 40;
+            int b = 20;
+            if(a < b)
+            {
+                Console.WriteLine( "a:{0}equal to b:{1}",a,b);
+            }
+            Console.WriteLine("{0} is greater than {1}", a,b);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,40 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SquareProblem
+namespace Loops
 {
-    class Square
+    class Program
     {
-        int a1=10;
-        int a2=10;
-        //public void Values()
-        //{
-          //  a1 = 4;
-            //a2 = 4;
-            
-        //}
-        public int GetArea()
-        {
-            return a1 * a2;
-        }
-        public void Display()
-        {
-            Console.WriteLine("side1:{0}", a1);
-            Console.WriteLine("side2:{0}", a2);
-            Console.WriteLine("Area of Square:{0}", GetArea());
-        }
-
         static void Main(string[] args)
         {
-            Square s = new Square();
-            Class1 c = new Class1();
-            //Assign a = new Assign(); 
-            //s.Values();
-            s.GetArea();
-            s.Display();
-            c.Check();
-           // a.Check();
-            Console.ReadLine();
+            Greater g = new Greater();
+            g.Method();
+            for (int i = 10; i <= 20; i = i + 1)
+            {
+                Console.WriteLine("value of i:{0}", i);
+
+            }
+                Console.ReadLine();
+            
+
         }
     }
 }
